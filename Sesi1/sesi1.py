11676716eed8e533a6304a80c426a52f23433346
gca98 <giovannichristian98@gmail.com>
2021-12-17 10:18:37 +0700

    init

---
+++ Sesi1/sesi1.py
@@ -0,0 +1,38 @@
+print(123123123123123123123123123123123123123123123123 + 1)
+print(10)
+print(type(10))
+
+print()
+
+print(4.2)
+print(type(4.2))
+print(4.)
+print(.2)
+print(.4e7)
+print(4.2e-4)
+
+print()
+
+print("Hacktiv8")
+print(type("Hacktiv8"))
+print("This string contains a single quote (') character.")
+print('This string contains a double quote (") character.')
+
+print()
+
+print(type(True))
+print(type(False))
+print(100 > 200) #False
+print(100 == 200) #False
+print(100 < 200) #True
+
+print()
+
+n = 300
+print(n)
+a = b = c = n
+print(a, b, c)
+
+print()
+
+
