625069bf26d679000f02efa0b2fc5222c09b1d51
gca98 <giovannichristian98@gmail.com>
2021-12-28 09:32:07 +0700

    make it better

--- Sesi11/config.py
+++ Sesi11/config.py
@@ -13,8 +13,8 @@ app = connex_app.app
 
 # Configure the SQLAlchemy part of the app instance
 app.config['SQLALCHEMY_ECHO'] = True
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://' + os.path.join(basedir, 'people.db')\
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jxvtxouzqpkyja:793d27257f563f8a0f95fd5da88ca38ebfcdddfee2ce749754ccf41feb95ce76@ec2-3-221-109-30.compute-1.amazonaws.com:5432/d31p51umh6fpgs'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'people.db')\
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jbgxhlbgynadfu:1f8af5489075c03cd4e89287a0e7b71f7cae6efe9825b5993362702c74942f72@ec2-52-72-252-211.compute-1.amazonaws.com:5432/d2snk4pv1ld6b7'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Create the SQLAlchemy db instance
